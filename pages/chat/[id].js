8013bb9754795bcf9f8c705cd2b29db17f91bfa1
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-02-19 22:56:16 +0000

    retrieving chat from server

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,7 +1,9 @@
+import { collection, doc, getDoc, orderBy, query } from "firebase/firestore";
 import Head from "next/head";
 import React from "react";
 import ChatScreen from "../../components/ChatScreen";
 import Sidebar from "../../components/Sidebar";
+import { db } from "../../firebase";
 
 function Chat() {
 	return (
@@ -10,7 +12,7 @@ function Chat() {
 				<title></title>
 			</Head>
 			<Sidebar />
-			<div className="flex-1">
+			<div className="flex-1 overflow-scroll h-screen">
 				<ChatScreen />
 			</div>
 		</div>
@@ -18,3 +20,26 @@ function Chat() {
 }
 
 export default Chat;
+
+export async function getServerSideProps(context) {
+    const ref = doc(collection(db, 'chats'), context.query.id)
+    const q = query(ref, orderBy('timestamp', 'asc'))
+
+    // Prep the messages on the server
+    const messageSnap = await getDoc(q, 'messages')
+    const messages = messageSnap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    })).map(messages => ({
+        ...messages,
+        timestamp: messages.timestamp.toDate().getTime()
+    }))
+
+    // Prep the chats
+    const chatSnap = await getDoc(q)
+    const chat = {
+        id: chatSnap.id,
+        ...chatSnap.data()
+    }
+
+}
